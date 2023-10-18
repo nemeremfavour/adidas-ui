a0acd0c4b36cb1d75ff95106337a2b72def28d97
Nemerem Favour <111649262+nemeremfavour@users.noreply.github.com>
2023-10-18 22:34:40 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,13 +5,13 @@ const button2 = document.querySelector('.wht');
 const button3 = document.querySelector('.red');
 
 button1.addEventListener('click', function(){
-  document.querySelector('.imgshoe').setAttribute('src', '/img/adidas-black.png');
+  document.querySelector('.imgshoe').setAttribute('src', 'img/adidas-black.png');
 });
 
 button2.addEventListener('click', function(){
-  document.querySelector('.imgshoe').setAttribute('src', '/img/adidas-white.png');
+  document.querySelector('.imgshoe').setAttribute('src', 'img/adidas-white.png');
 });
 
 button3.addEventListener('click', function(){
-  document.querySelector('.imgshoe').setAttribute('src', '/img/adidas-red.png');
+  document.querySelector('.imgshoe').setAttribute('src', 'img/adidas-red.png');
 });
